5cd70a842757ba7a39d9bc2c9ff8c65d2ea98761
rohitkumar112 <rohitkumar112.rex@gmail.com>
2021-02-25 17:16:57 +0530

    first commit

---
+++ src/components/UseRef.js
@@ -0,0 +1,19 @@
+import React, { useRef } from 'react'
+
+function UseRef() {
+    const inputEl = useRef(null);
+    const onButtonClick = () => {
+        console.log(inputEl);
+      // `current` points to the mounted text input element
+      inputEl.current.focus();
+    };
+    return (
+      <>
+        <input ref={inputEl} type="text" />
+        <button onClick={onButtonClick}>Focus the input</button>
+      </>
+    );
+  }
+  
+
+export default UseRef;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UseRef from "./components/UseRef";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UseRef />
   </React.StrictMode>,
   document.getElementById('root')
 );
